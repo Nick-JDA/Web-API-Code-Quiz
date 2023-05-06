b411bdbfcece199e01a333faf8ea979c7b340ce4
Nick JDA <armstrongnick1999@gmail.com>
2023-05-06 12:04:13 -0400

    Added basic layout to HTML, added pseudo code to JavaScript for quiz loop

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//quiz loop
+    //title card with description and start quiz button
+        //75 second timer starts on start quiz button push
+        //first question is shown with possible answers
+            //user answers and next question is shown
+                //if correct answer, message at bottom of question card states correct and no timer change
+                //if incorrect answer, message at bottom of question card states incorrect and timer is subtracted from ex -20 seconds
+            //after last question is asked timer stops
+                //card with title saying "all done" appears 
+                //initial entry form with score displayed and submit button to record score and initials of user
+            //once score submit button pressed switch to Highscore card that has list of all scores and initials
+                //button with "clear highscores" displayed
+                    //upon press highscore list is cleared
+                //button with "go back" is displayed
+                    //upon press user is brought back to title card
+    //loop restarts
+    
